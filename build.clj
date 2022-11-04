c7a3ffcda70cf27a3bb9cb17655cf9483a427959
John Shaffer <jdsha@proton.me>
2022-11-04 12:17:22 -0500

    Use build-clj for testing and releasing

---
+++ build.clj
@@ -0,0 +1,27 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [org.corfield.build :as bb]))
+
+(def lib 'io.staticweb/rate-limit)
+(def version "0.3.0")
+(defn get-version [opts]
+  (str version (when (:snapshot opts) "-SNAPSHOT")))
+
+(defn test [opts]
+  (-> opts
+      (bb/run-tests))
+  opts)
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/clean
+      test
+      bb/clean
+      (assoc :src-pom "template/pom.xml")
+      bb/jar))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/deploy))
